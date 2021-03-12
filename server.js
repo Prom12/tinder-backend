2805e608d8ff9d90077476129adca5ad543b9731
prom12 <51293420+Prom12@users.noreply.github.com>
2021-03-12 17:54:35 +0000

    editing schema

---
+++ server.js
@@ -0,0 +1,52 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import Cors from 'cors'
+
+import card from './dbCards.js'
+const connection_url = "mongodb+srv://admin:KLwS3Rp4cGh62r4i@cluster0.q9cfs.mongodb.net/portfolio_db?retryWrites=true&w=majority"
+
+//App config
+const app = express();
+let port = process.env.PORT || '3000';
+
+//middleware
+app.use(express.json());
+app.use(Cors());
+
+//listener
+app.listen(port,()=>console.log(`listening on localhost: ${port}`));
+
+//DB Config
+mongoose.connect(connection_url,{
+    useNewUrlParser: true,
+    useCreateIndex:true,
+    useUnifiedTopology: true,
+  })
+  
+app.get('/', (req,res,next)=>{
+    res.status(200).send('Romeo')
+})
+
+app.post('/card',(req,res)=>{
+    const dbCard = req.body;
+
+    card.create(dbCard,(err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }
+        else{
+            res.status(201).send(data)
+        }
+    })
+})
+
+app.get('/card',(req,res)=>{
+    card.find((err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }
+        else{
+            res.status(200).send(data)
+        }
+    })
+})
